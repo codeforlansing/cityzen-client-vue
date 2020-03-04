b7b2b9d154afaabc9576a9038d9baeeb3286eac2
yusuke <yusuken64@gmail.com>
2020-03-04 18:30:30 -0500

    removed files

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <Test/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Test from './components/Test';
+
+export default {
+  name: 'App',
+
+  components: {
+    Test,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-sheet height="400">
+        <v-calendar
+          ref="calendar"
+          :now="today"
+          :value="today"
+          :events="events"
+          color="primary"
+          type="month"
+        ></v-calendar>
+      </v-sheet>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
