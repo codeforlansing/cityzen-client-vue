70ad00d91211af67a847cb2520033618061b0fab
Erik Gillespie <erik.gillespie@gmail.com>
2020-05-15 15:58:37 -0400

    Render tasks as 3-column set of checkboxes

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="cfl-task-list">
-    <ol>
-      <li v-for="task in tasks" :key="task.taskId">{{ task.name }}</li>
-    </ol>
-  </div>
+  <fieldset class="cfl-task-list">
+    <legend>Volunteer Opportunities</legend>
+    <ul>
+      <li v-for="task in tasks" :key="task.taskId">
+        <div>
+          <input
+            type="checkbox"
+            :id="task.taskId"
+            :value="task.taskId"
+            v-model="checkedTaskIds"
+          >
+          <label :for="task.taskId">{{ task.name }}</label>
+        </div>
+      </li>
+    </ul>
+  </fieldset>
 </template>
 
 <script lang="ts">
@@ -17,26 +28,28 @@ export default Vue.extend({
       type: Array as PropType<Array<Task>>,
       required: true
     }
+  },
+  data () {
+    return {
+      checkedTaskIds: []
+    }
   }
 })
 </script>
 
 <style>
-.cfl-task-list h3 {
-  margin: 40px 0 0;
+.cfl-task-list > ul {
+  columns: 12rem 3;
+  list-style: none;
+  padding-inline-start: 0;
 }
 
-.cfl-task-list ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-.cfl-task-list li {
+.cfl-task-list > ul > li {
   display: inline-block;
-  margin: 0 10px;
 }
 
-.cfl-task-list a {
-  color: #42b983;
+.cfl-task-list > ul > li > div {
+  display: flex;
+  align-items: flex-start;
 }
 </style>
