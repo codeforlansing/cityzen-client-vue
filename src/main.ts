36bf73066cda7e9c1f8d26890141e3edda10564f
Erik Gillespie <erik.gillespie@gmail.com>
2020-05-20 13:14:22 -0400

    Configurable tasks endpoint and better error handling/logging

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App)
-}).$mount('#cfl-volunteer-tasks')
+const mountElement = document.getElementById('cfl-volunteer-tasks')
+if (!mountElement) {
+  console.warn(
+    'Unable to render Cityzen client.',
+    '#cfl-volunteer-tasks element does not exist.'
+  )
+} else {
+  new Vue({
+    render (createElement) {
+      return createElement(App, {
+        props: {
+          tasksHref: mountElement.dataset.tasksHref
+        }
+      })
+    }
+  }).$mount(mountElement)
+}
