5b1d8fab591ef6860030c446e9d3e847a5ea15d7
Robin M <root@grenewode.io>
2020-06-03 19:52:01 -0400

    Add support for cmd line args & use npm-run-all
    You can now launch the application using `npm start --config
    trello.json`, which will make it easier to customize things
    transparently.
    
    Additionally, this refactors to use npm-run-all, which provides a nice,
    cross platform solution for launching multiple commands in parallel with
    nodejs

--- bin/launch-cityzen-server.js
+++ bin/launch-cityzen-server.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 
 require('dotenv').config()
-const child_process = require('child_process')
+const ChildProcess = require('child_process')
 
-console.log('starting cityzen server...')
+const argv = process.argv.slice(process.argv.indexOf(__filename) + 1)
 
-let { CITYZEN_SERVER_AUTOLAUNCH_BRANCH, CITYZEN_SERVER_AUTOLAUNCH_URL } = process.env
+let { CITYZEN_SERVER_AUTOLAUNCH_BRANCH, CITYZEN_SERVER_AUTOLAUNCH_URL, CITYZEN_SERVER_USE_CACHED } = process.env
 
 CITYZEN_SERVER_AUTOLAUNCH_BRANCH = CITYZEN_SERVER_AUTOLAUNCH_BRANCH || 'master'
 CITYZEN_SERVER_AUTOLAUNCH_URL = CITYZEN_SERVER_AUTOLAUNCH_URL || `git://github.com/codeforlansing/cityzen-server.git#${CITYZEN_SERVER_AUTOLAUNCH_BRANCH}`
+CITYZEN_SERVER_USE_CACHED = CITYZEN_SERVER_USE_CACHED !== 'false'
 
-child_process.spawn('npx', ['--ignore-existing=false', CITYZEN_SERVER_AUTOLAUNCH_URL], { stdio: 'inherit' })
+console.log('starting cityzen server...')
+ChildProcess.spawn('npx', [`--ignore-existing=${!CITYZEN_SERVER_USE_CACHED}`, `--package=${CITYZEN_SERVER_AUTOLAUNCH_URL}`, 'cityzen-server'], { stdio: 'inherit' })
