bab8d7529d4ebf2122989e3bc9af832b77452aed
Erik Gillespie <erik.gillespie@gmail.com>
2020-06-17 20:29:45 -0400

    Allow clients to override messages

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import messages from './default-messages.json'
+import defaultMessages from './default-messages.json'
+import merge from 'lodash/merge'
 
 Vue.use(VueI18n)
+
+// Initialize default messages
+const messages = Object.assign({}, defaultMessages)
+
+// Attempt to load and use client messages
+const clientMessagesScript = document.getElementById('cfl-cityzen-messages')
+if (clientMessagesScript) {
+  try {
+    const clientMessagesJson = clientMessagesScript.textContent
+    const clientMessages = JSON.parse(clientMessagesJson)
+    merge(messages, clientMessages)
+  } catch (error) {
+    console.warn(
+      'cfl-cityzen-messages exists but cannot be read. Using defaults instead.',
+      error
+    )
+  }
+}
 
 const i18n = new VueI18n({
   locale: 'en',
